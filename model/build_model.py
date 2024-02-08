b8f898fc3be303cea6e472e48f6baae5836a7802
Your Name <you@example.com>
2024-02-09 00:46:01 +0700

    fix: label is not one-hot encoded, so changing loss  function and activation function in last layer

--- model/build_model.py
+++ model/build_model.py
@@ -1,11 +1,10 @@
 import tensorflow as tf
-from tensorflow_addons.metrics import F1Score
 from keras.models import Sequential
 from keras.layers import Conv2D, Dense, Flatten, Resizing, MaxPooling2D, Input, Resizing, Rescaling
 from keras.metrics import Recall, Precision, Accuracy
 from keras.callbacks import EarlyStopping, ModelCheckpoint, LearningRateScheduler
 from keras.optimizers import Adam
-from keras.losses import CategoricalCrossentropy
+from keras.losses import SparseCategoricalCrossentropy
 
 Dataset = tf.data.Dataset
 AUTOTUNE = tf.data.AUTOTUNE
@@ -22,30 +21,30 @@ def lr_time_based_decay(epoch, lr):
     return lr * 1 / (1 + decay * epoch)
 
 
-def create_model(resize_shape: tuple = (32, 32), initial_learning_rate: float = 0.0001):
+def create_model(resize_shape: list = [32, 32], initial_learning_rate: float = 0.0001):
     model = Sequential()
     model.add(Rescaling(1. / 255, input_shape=(384, 384, 3)))
-    model.add(Resizing(resize_shape[0], resize_shape[1]))
+    model.add(Resizing(height=resize_shape[0], width=resize_shape[1]))
     model.add(Conv2D(filters=32, kernel_size=(3, 3), activation='relu'))
     model.add(MaxPooling2D())
     model.add(Conv2D(filters=64, kernel_size=(3, 3), activation='relu'))
     model.add(MaxPooling2D())
     model.add(Flatten())
     model.add(Dense(128, activation='relu'))
-    model.add(Dense(units=4))
+    model.add(Dense(units=4, activation='softmax'))
 
-    model.compile(optimizer=Adam(learning_rate=initial_learning_rate), loss=CategoricalCrossentropy(),
-                  metrics=[Recall(), Precision(), F1Score(), Accuracy()])
+    model.compile(optimizer=Adam(learning_rate=initial_learning_rate), loss='sparse_categorical_crossentropy',
+                  metrics=[Recall(), Precision(), Accuracy()])
 
     return model
 
 
-def fit_model(model, epochs, train, val):
+def fit_model(model, train, val, epochs: int = 1000):
     history = model.fit(train,
                         validation_data=val,
                         epochs=epochs,
                         verbose=50,
                         callbacks=[early_stopping, model_checkpoint,
                                    LearningRateScheduler(schedule=lr_time_based_decay, verbose=1)])
-    model = tf.keras.models.load_model('best_model.h5')
-    return model, history
+    load_model = tf.keras.models.load_model('best_model.h5')
+    return load_model, history

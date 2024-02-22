d1b8b0805ac42e1552689e5dbb997709ff18f94e
Your Name <you@example.com>
2024-02-22 16:19:19 +0700

    feat: create function helping OneHotEncoding label

--- data/preprocessing.py
+++ data/preprocessing.py
@@ -20,10 +20,14 @@ def load_data(data_path: str) -> Dataset:
     relaxed_images = Dataset.list_files(os.path.join(data_path, emotions[2]) + '/*.jpg')
     sad_images = Dataset.list_files(os.path.join(data_path, emotions[3]) + '/*.jpg')
 
-    angry_with_label = Dataset.zip((angry_images, Dataset.from_tensor_slices(tf.ones(len(angry_images)))))
-    happy_with_label = Dataset.zip((happy_images, Dataset.from_tensor_slices(2 * tf.ones(len(happy_images)))))
-    relaxed_with_label = Dataset.zip((relaxed_images, Dataset.from_tensor_slices(3 * tf.ones(len(relaxed_images)))))
-    sad_with_label = Dataset.zip((sad_images, Dataset.from_tensor_slices(4 * tf.ones(len(sad_images)))))
+    angry_with_label = Dataset.zip(
+        (angry_images, Dataset.from_tensor_slices([one_hot_encoding(emotions[0]) for _ in range(len(angry_images))])))
+    happy_with_label = Dataset.zip(
+        (happy_images, Dataset.from_tensor_slices([one_hot_encoding(emotions[1]) for _ in range(len(happy_images))])))
+    relaxed_with_label = Dataset.zip((relaxed_images, Dataset.from_tensor_slices(
+        [one_hot_encoding(emotions[2]) for _ in range(len(relaxed_images))])))
+    sad_with_label = Dataset.zip(
+        (sad_images, Dataset.from_tensor_slices([one_hot_encoding(emotions[3]) for _ in range(len(sad_images))])))
 
     data = angry_with_label.concatenate(happy_with_label).concatenate(relaxed_with_label).concatenate(sad_with_label)
     return data
@@ -33,6 +37,20 @@ def train_val_split(data: Dataset, train_rate: float = 0.8):
     train_ds = data.take(int(len(data) * train_rate))
     val_ds = data.skip(int(len(data) * train_rate)).take(len(data) - len(train_ds))
     return train_ds, val_ds
+
+
+def one_hot_encoding(emotion):
+    res = []
+    match emotion:
+        case 'angry':
+            res = [1.0, 0.0, 0.0, 0.0]
+        case 'happy':
+            res = [0.0, 1.0, 0.0, 0.0]
+        case 'relaxed':
+            res = [0.0, 0.0, 1.0, 0.0]
+        case 'sad':
+            res = [0.0, 0.0, 0.0, 1.0]
+    return res
 
 
 def preprocess(file_path, label):

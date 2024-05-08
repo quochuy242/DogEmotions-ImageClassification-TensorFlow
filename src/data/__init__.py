f19c26b64266edf878420f5e474518943b30c6fe
Huy Truong <truongquochuy.240203@gmail.com>
2024-05-08 21:32:27 +0700

    update training phase

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -1,18 +1,11 @@
 import tensorflow as tf
 import os
-import src
 import opendatasets as od
-import cv2
-import numpy as np
 import shutil
-import zipfile
-import pickle
+import os.path as osp
 
-from tensorflow import keras
 from pathlib import Path
-from typing import Tuple
-
-import src.logging
+from src import logging
 
 
 class DataIngestion:
@@ -29,34 +22,23 @@ class DataIngestion:
     @property
     def download_data(self):
         if self.download:
-            src.logging.info("Downloading data...")
-            if os.path.exists(self.data_path / "fer2013"):
+            logging.info("Downloading data...")
+            if osp.exists(self.data_path / "fer2013"):
                 shutil.rmtree(self.data_path / "fer2013")
             od.download(self.url_download, self.data_path)
             self.download = False
-            src.logging.info("Downloaded data!!")
+            logging.info("Downloaded data!!")
 
-            # with zipfile.ZipFile(
-            #     self.data_path / "fer2013" / "fer2013.zip", "r"
-            # ) as zip_ref:
-            #     zip_ref.extractall(self.data_path / "fer2013")
-
-        if os.path.exists(self.data_path / "fer2013" / "test" / "disgust"):
+        if osp.exists(self.data_path / "fer2013" / "test" / "disgust"):
             shutil.rmtree(self.data_path / "fer2013" / "test" / "disgust")
             shutil.rmtree(self.data_path / "fer2013" / "train" / "disgust")
-        # if os.path.exists(self.data_path / "fer2013"):
-        #     shutil.copytree(
-        #         self.data_path / "fer2013" / "test",
-        #         self.data_path,
-        #         dirs_exist_ok=True,
-        #     )
-        #     shutil.copytree(
-        #         self.data_path / "fer2013" / "train",
-        #         self.data_path,
-        #         dirs_exist_ok=True,
-        #     )
-        # shutil.rmtree(self.data_path / "fer2013")
-        src.logging.info("Prepared dataset!!")
+
+        if osp.exists(self.data_path / "fer2013"):
+            shutil.move(self.data_path / "fer2013" / "test", self.data_path)
+            shutil.move(self.data_path / "fer2013" / "train", self.data_path)
+            shutil.rmtree(self.data_path / "fer2013")
+
+        logging.info("Prepared dataset!!")
 
 
 class DataTransformation:
@@ -64,104 +46,46 @@ class DataTransformation:
         self.image_size = (
             config["image_height"],
             config["image_width"],
-            config["image_channels"],
         )
-        self.num_classes = config["num_classes"]
-        self.train_ratio = config["train_ratio"]
-        self.test_ratio = config["test_ratio"]
-        self.val_ratio = config["val_ratio"]
-        self.save_path = Path(config["save_path"])
-
-    def get_label(self, path: Path) -> str:
-        return path.parent.name
-
-    def one_hot_encode(self, label: str) -> int:
-        map_label = {
-            "angry": 0,
-            "fear": 1,
-            "happy": 2,
-            "neutral": 3,
-            "sad": 4,
-            "surprise": 5,
-        }
-        label = map_label[label]
-        return tf.one_hot(label, depth=6)
-
-    def get_image(self, path: Path) -> np.ndarray[float]:
-        img = cv2.imread(str(path))
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        img = cv2.resize(
-            img, dsize=self.image_size[:-1]
-        )
-        img = img.astype(np.float32) / 255.0
-        img = keras.utils.img_to_array(img)
-        img = np.expand_dims(img, axis=-1)
-        return img
-
-    def get_dataset(self, path: Path) -> tf.data.Dataset:
-
-        src.logging.info(f"Loading dataset from: {path}")
-        list_labels = os.listdir(path)
-
-        list_images = []
-        try:
-            for label in list_labels:
-                label_dir = path / label
-                path_images = [
-                    label_dir / f for f in os.listdir(label_dir) if f.endswith(".jpg")
-                ]
-                list_images.extend(path_images)
-            images = list(map(self.get_image, list_images))
-            labels = list(map(self.get_label, list_images))
-            labels = list(map(self.one_hot_encode, labels))
-        except Exception as e:
-            src.logging.exception(e)
-
-        dataset = tf.data.Dataset.from_tensor_slices((images, labels))
-        N = len(dataset)
-        dataset = dataset.shuffle(buffer_size=N + 1)
-
-        train_size = int(N * self.train_ratio)
-        test_size = int(N * self.test_ratio)
-        val_size = int(N * self.val_ratio)
-        train_ds = dataset.take(train_size)
-        test_ds = dataset.skip(train_size).take(test_size)
-        val_ds = dataset.skip(train_size + test_size).take(val_size)
-
-        src.logging.info(f"Number of train samples: {len(train_ds)}")
-        src.logging.info(f"Number of val samples: {len(val_ds)}")
-        src.logging.info(f"Number of test samples: {len(test_ds)}")
-        return train_ds, test_ds, val_ds
-
-    def save(self, dataset: tf.data.Dataset, path: str) -> None:
-        # Create directory if not exists
-        save_dir = self.save_path / path
-        save_dir.mkdir(parents=True, exist_ok=True)
-        
-        # Save dataset to TFRecord files
-        save_dir = str(save_dir)
-        dataset.save(save_dir, compression='GZIP')
-        with open(save_dir + '/element_spec', 'wb') as out_:
-            pickle.dump(dataset.element_spec, out_)
-        src.logging.info(f'Saved dataset to: "{save_dir}"')
-
-
-class DataLoader:
-    def __init__(self, config):
-        self.load_path = config["load_path"]
+        self.image_channels_expactation = config["image_channels_expectation"]
+        self.class_names = []
         self.batch_size = config["batch_size"]
 
-    def load(self, ds_name: str = "train"):
-        match ds_name.lower():
-            case "train":
-                ds_path = os.path.join(self.load_path, "train")
-            case "test":
-                ds_path = os.path.join(self.load_path, "test")
-            case "val":
-                    ds_path = os.path.join(self.load_path, "test")
-        
-        with open(ds_path + "/element_spec", "rb") as in_:
-            es = pickle.load(in_)
-        ds = tf.data.Dataset.load(ds_path, es, compression='GZIP')
-        ds = ds.batch(batch_size=self.batch_size).cache().prefetch(tf.data.AUTOTUNE)
-        return ds
+    def get_dataset(self, path: Path) -> tuple[tf.data.Dataset]:
+
+        logging.info(f"Loading dataset from: {path}")
+        train_ds = tf.keras.utils.image_dataset_from_directory(
+            path / "train",
+            seed=242,
+            image_size=self.image_size,
+            batch_size=self.batch_size,
+            shuffle=True,
+            label_mode="categorical",
+            color_mode="grayscale" if self.image_channels_expactation == 1 else "rgb",
+            interpolation="gaussian",
+            validation_split=0.1,
+            subset="training",
+        )
+        val_ds = tf.keras.utils.image_dataset_from_directory(
+            path / "test",
+            seed=242,
+            image_size=self.image_size,
+            batch_size=self.batch_size,
+            shuffle=True,
+            label_mode="categorical",
+            color_mode="grayscale" if self.image_channels_expactation == 1 else "rgb",
+            interpolation="gaussian",
+            validation_split=0.1,
+            subset="validation",
+        )
+        self.class_name = train_ds.class_names
+
+        for image, label in train_ds:
+            logging.info(f"Batched image shape: {image.shape}")
+            logging.info(f"Batched label shape: {label.shape}")
+            break
+
+        AUTOTUNE = tf.data.AUTOTUNE
+        train_ds = train_ds.cache().prefetch(buffer_size=AUTOTUNE)
+        val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+        return train_ds, val_ds
--- src/model/__init__.py
+++ src/model/__init__.py
@@ -1,23 +1,32 @@
 import os
 import tensorflow as tf
 import matplotlib.pyplot as plt
-from tensorflow import keras
-from keras import Sequential, layers, optimizers, losses, metrics, callbacks
+import src
+import keras
+from keras import (
+    Sequential,
+    layers,
+    optimizers,
+    losses,
+    metrics,
+    callbacks,
+    regularizers,
+)
 from pathlib import Path
-from src import logging, read_yaml
 from sklearn.metrics import ConfusionMatrixDisplay, confusion_matrix
 
-params = read_yaml(Path("params.yaml"))
+params = src.read_yaml(Path("params.yaml"))
 
 data_augmentation = Sequential(
     [
+        layers.Rescaling(1.0 / 255),
         layers.RandomFlip("horizontal"),
         layers.RandomRotation(0.1),
         layers.RandomZoom(0.1),
-    ]
+    ],
+    name="data_augmentation",
 )
 
-data_augmentation._name = "augmentation"
 
 early_stop = callbacks.EarlyStopping(
     monitor=params["early_stopping"]["monitor"],
@@ -36,17 +45,48 @@ reduce_lr = callbacks.ReduceLROnPlateau(
 )
 
 
-def evaluate(model, test_ds):
+def evaluate_model(model: keras.Sequential, test_ds: tf.data.Dataset):
     result = model.evaluate(test_ds)
-    logging.info(f"Evaluating {model._name} model: {result}")
+    src.logging.info(f"Evaluating {model._name} model: {result}")
 
     y_pred = model.predict(test_ds)
     y_pred = y_pred.argmax(axis=1)
 
     y_true = test_ds.map(lambda x, y: y)
     ConfusionMatrixDisplay(confusion_matrix(y_true, y_pred)).plot()
+    os.makedirs(name=f"visualize/{model._name}", exist_ok=True)
     plt.savefig(f"visualize/{model._name}/confusion_matrix.png")
     return result
+
+
+def fit_model(
+    model: keras.Sequential,
+    train_ds: tf.data.Dataset,
+    val_ds: tf.data.Dataset,
+    epochs: int = 10,
+) -> callbacks.History:
+    history = model.fit(
+        train_ds,
+        epochs=epochs,
+        validation_data=val_ds,
+        callbacks=[
+            early_stop,
+            reduce_lr,
+            callbacks.ModelCheckpoint(
+                filepath=f"weights/{model._name}/best.keras",
+                monitor="val_f1_score",
+                save_best_only=True,
+                verbose=0,
+            ),
+            callbacks.ModelCheckpoint(
+                filepath=f"weights/{model._name}/last.keras",
+                monitor="val_loss",
+                verbose=0,
+            ),
+            callbacks.CSVLogger("logs/" + model._name + ".log"),
+        ],
+    )
+    return history
 
 
 class CNN:
@@ -59,8 +99,6 @@ class CNN:
         self.initial_lr = params["learning_rate"]
         self.l1 = params["l1"]
         self.l2 = params["l2"]
-        self.epochs = params["epochs"]
-        self.model = None
 
     @property
     def build_model(self) -> Sequential:
@@ -69,23 +107,21 @@ class CNN:
         )
 
         for units in self.conv_units:
-            model.add(layers.Conv2D(units, 3, activation='relu', padding="same"))
+            model.add(layers.Conv2D(units, 3, activation="relu", padding="same"))
             model.add(layers.BatchNormalization())
             model.add(layers.MaxPooling2D(pool_size=(2, 2)))
             model.add(layers.Dropout(self.dropout_rate))
 
         model.add(layers.Flatten())
         for units in self.dense_units:
-            model.add(layers.Dense(units, activation='relu'))
+            model.add(layers.Dense(units, activation="relu"))
             model.add(layers.Dropout(self.dropout_rate))
 
         model.add(
             layers.Dense(
                 self.num_classes,
-                activation='softmax',
+                activation="softmax",
                 name="output",
-                # kernel_regularizer=tf.keras.regularizers.l1(self.l1),
-                # activity_regularizer=tf.keras.regularizers.l2(self.l2),
             ),
         )
         model.compile(
@@ -100,41 +136,16 @@ class CNN:
             ],
         )
         model._name = "CNN"
-        logging.info(f"{model._name} model summary: ")
-        logging.info(model.summary())
-        os.makedirs(name=f'visualize/{model._name}', exist_ok=True)
+        src.logging.info(f"{model._name} model summary: ")
+        src.logging.info(model.summary())
+        os.makedirs(name=f"visualize/{model._name}", exist_ok=True)
         keras.utils.plot_model(
             model,
             to_file=f"visualize/{model._name}/layers_structure.png",
             show_shapes=True,
             show_layer_names=True,
         )
-        self.model = model
         return model
-
-    def fit_model(self, train_ds, val_ds):
-        history = self.model.fit(
-            train_ds,
-            epochs=self.epochs,
-            validation_data=val_ds,
-            callbacks=[
-                early_stop,
-                reduce_lr,
-                callbacks.ModelCheckpoint(
-                    filepath=f"weights/{self.model._name}/best.keras",
-                    monitor="val_f1_score",
-                    save_best_only=True,
-                    verbose=0,
-                ),
-                callbacks.ModelCheckpoint(
-                    filepath=f"weights/{self.model._name}/last.keras",
-                    monitor="val_loss",
-                    verbose=0,
-                ),
-                callbacks.CSVLogger("logs/" + self.model._name + ".log"),
-            ],
-        )
-        return history
 
 
 class MLP:
@@ -147,7 +158,6 @@ class MLP:
         self.l1 = params["l1"]
         self.l2 = params["l2"]
         self.epochs = params["epochs"]
-        self.model = None
 
     @property
     def build_model(self) -> Sequential:
@@ -178,8 +188,8 @@ class MLP:
             ],
         )
         model._name = "MLP"
-        logging.info(f"{model._name} model summary: ")
-        logging.info(model.summary())
+        src.logging.info(f"{model._name} model summary: ")
+        src.logging.info(model.summary())
         keras.utils.plot_model(
             model,
             to_file=f"visualize/{model._name}.png",
@@ -188,30 +198,6 @@ class MLP:
         )
         self.model = model
         return model
-
-    def fit_model(self, train_ds, val_ds) -> callbacks.History:
-        history = self.model.fit(
-            train_ds,
-            epochs=self.epochs,
-            validation_data=val_ds,
-            callbacks=[
-                early_stop,
-                reduce_lr,
-                callbacks.ModelCheckpoint(
-                    filepath=f"weights/{self.model._name}/best.keras",
-                    monitor="val_f1_score",
-                    save_best_only=True,
-                    verbose=0,
-                ),
-                callbacks.ModelCheckpoint(
-                    filepath=f"weights/{self.model._name}/last.keras",
-                    monitor="val_loss",
-                    verbose=0,
-                ),
-                callbacks.CSVLogger("logs/" + self.model._name + ".log"),
-            ],
-        )
-        return history
 
 
 class ViT:

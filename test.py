e07fa66ee6c410c6f92c79d80a1aaa591acfeeec
Huy Truong <truongquochuy.240203@gmail.com>
2024-05-22 17:05:17 +0700

    feat: update weight of CNN model

--- test.py
+++ test.py
@@ -1,57 +1,67 @@
-from isoduration import DurationFormattingException
+import tensorflow as tf
 import keras
 import os
 import warnings
 import cv2
 import numpy as np
 import time
+import src
 from pathlib import Path
 from tqdm import tqdm, trange
+from src import logging
+
+osp = os.path
 
 
-def load(filename: Path) -> np.ndarray:
-    img = cv2.imread(filename)
-    if img.ndim == 3:
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    return img
+def load_image_file(path, **kwargs):
+    try:
+        img = keras.utils.load_img(path, **kwargs)
+    except Exception as e:
+        logging.exception(e)
+    arr = keras.utils.img_to_array(img)
+    arr = arr / 255.0
+
+    if len(arr.shape) == 2:
+        arr = np.expand_dims(arr, axis=-1)
+    arr = np.array([arr])
+    return arr
 
 
-def preprocesing(img: np.ndarray) -> np.ndarray:
-    img = cv2.resize(img, dsize=(128, 128))
-    img = img / 255.0
-    img = np.expand_dims(img, axis=-1)
-    return img
+def load_image(path: str, **kwargs):
+    if osp.isfile(path):
+        return load_image_file(path, **kwargs)
+    elif osp.isdir(path):
+        arrs = []
+        file = os.listdir(path)
+        for f in file:
+            if f[-4:] not in [".jpg", ".png"]:
+                continue
+            else:
+                arr = load_image_file(f"{path}/{f}", **kwargs)
+                arrs.append(arr)
+
+        return np.array(arrs)
+    else:
+        src.logging.error(f"{path} is not a file or directory")
+        return None
 
 
 def main():
-
     # Load models from weights directory
     model = keras.models.load_model("weights/CNN/best.keras")
+    images = load_image("dataset/test", color_mode="grayscale")
 
-    # Load images
-    print("Loading images...")
-    images = []
-    classes = os.listdir("dataset/test")
-    for classname in tqdm(classes):
-        filenames = os.listdir(f"dataset/test/{classname}")
-        for filename in tqdm(filenames):
-            if filename[-4:] not in [".jpg", ".png"]:
-                continue
-            path = f"dataset/test/{classname}/{filename}"
-            image = load(path)
-            image = preprocesing(image)
-            images.append(image)
+    print(images.shape)
+    # # Predicting the images
+    # durations = []
+    # print("Predicting the images...")
+    # for image in tqdm(images):
+    #     start = time.time()
+    #     model.predict(image)
+    #     durations.append(time.time() - start)
 
-    # Predicting the images
-    durations = []
-    print("Predicting the images...")
-    for image in tqdm(images):
-        start = time.time()
-        model.predict(image)
-        durations.append(time.time() - start)
-
-    # Calc mean of time prediction
-    print(f"Mean of time prediction: {np.mean(durations)}")
+    # # Calc mean of time prediction
+    # print(f"Mean of time prediction: {np.mean(durations)}")
 
 
 if __name__ == "__main__":
